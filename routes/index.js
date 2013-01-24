ca455b6a5364b1b14759a8ef40b3a081e230b01d
yonishiy <nishiyama2002@hotmail.com>
2013-01-24 12:42:53 +0900

    bu fix

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-
 /*
  * GET home page.
  */
@@ -6,28 +5,20 @@
 var items = [
     { "text": "1st Post." }
     , { "text": "2nd Post." }
-];*/
+];
+
 var model = require('../model');
 var Post = model.Post;
-
+*/
 exports.index = function(req, res){
-    Post.find({}, function(err, items){
-        res.render('index', { title: 'Entry List', items: items })
-    });
+    res.render('index', { title: 'Entry List', items: items })
 };
 
 exports.form = function(req, res){
-    res.render('form', { title: 'Form Entry' })
+    res.render('form', { title: 'New Entry' })
 };
 
 exports.create = function(req, res){
-    var newPost = new Post(req.body);
-    newPost.save(function(err){
-        if (err) {
-            console.log(err);
-            res.redirect('back');
-        } else {
-            res.redirect('/');
-        }
-    });
+    console.log(req.body.text);
+    res.redirect('/');
 };
